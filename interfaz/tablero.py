8e1ace7d11707d7d101ce9c1249621f9e77e4d4f
John Chavarría <j.chavarria.3@estudiantec.cr>
2026-06-21 12:54:23 -0600

    Tablero grafico conectado al mapa

--- interfaz/tablero.py
+++ interfaz/tablero.py
@@ -1,11 +1,18 @@
 import tkinter as tk
 
+from modelos.base import Base
+from modelos.muro import Muro
+from modelos.torre import Torre
+from modelos.unidad import Unidad
+
+
 class Tablero:
 
-    def __init__(self):
+    def __init__(self, mapa):
+
+        self.mapa = mapa
 
         self.ventana = tk.Tk()
-
         self.ventana.title("Defensa y Asalto de Base")
 
         self.botones = []
@@ -32,4 +39,32 @@ class Tablero:
 
             self.botones.append(fila_botones)
 
+        self.dibujar_mapa()
+
         self.ventana.mainloop()
+
+    def dibujar_mapa(self):
+
+        for fila in range(10):
+
+            for columna in range(10):
+
+                objeto = self.mapa.matriz[fila][columna]
+
+                texto = ""
+
+                if isinstance(objeto, Base):
+                    texto = "B"
+
+                elif isinstance(objeto, Muro):
+                    texto = "M"
+
+                elif isinstance(objeto, Torre):
+                    texto = "T"
+
+                elif isinstance(objeto, Unidad):
+                    texto = "U"
+
+                self.botones[fila][columna].config(
+                    text=texto
+                )
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from modelos.mapa import Mapa
+from modelos.muro import Muro
+from modelos.torre import TorreArquero
+from modelos.unidad import Soldado
+
 from interfaz.tablero import Tablero
 
-Tablero()
+mapa = Mapa()
+
+mapa.colocar_objeto(4, 5, Muro())
+mapa.colocar_objeto(5, 4, TorreArquero())
+mapa.colocar_objeto(2, 2, Soldado())
+
+Tablero(mapa)
